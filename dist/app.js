3aad762fa2222b94f3d3965eae0a3811e66c3a76
subas-roy <subasroy46@gmail.com>
2024-11-16 01:46:14 +0600

    Usage of parsers, request and response object

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// parsers
+app.use(express_1.default.json());
+app.use(express_1.default.text());
 app.get('/', (req, res) => {
     res.send('Hello world!');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    // res.send('got data') 
+    res.json({
+        "message": "Successfully received data"
+    });
 });
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 const app = express()
 
-app.get('/', (req, res) => {
+// parsers
+app.use(express.json());
+app.use(express.text());
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!')
+})
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body)
+  // res.send('got data') 
+  res.json({
+    "message": "Successfully received data"
+  })
 })
 
 export default app;
